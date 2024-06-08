9ecd3021142dc531bf5368907e2d6b8a4afe02ac
Josieljcc <josiel.jcc@hotmail.com>
2024-06-08 15:06:35 -0300

    refactor: create routes file

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import Login from "./pages/login/index.tsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-]);
+import { RouterProvider } from "react-router-dom";
+import router from "./routes/routes";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
