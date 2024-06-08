c1d819835d86b939db7d83e5a8679dcef0ff3e81
Josieljcc <josiel.jcc@hotmail.com>
2024-06-08 15:21:57 -0300

    chore: configure tailwindcss

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,5 @@
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  return <div className="bg-slate-800 text-white h-[100vh]">Login</div>;
+};
 
-export default Login
+export default Login;
