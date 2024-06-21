ee6d3dbac885b30d430fc3f864f491670873245a
cwesly <cauawesly@outlook.com>
2024-06-21 10:46:01 -0300

    refactor: remove "/login" route

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,15 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
-import App from "../App";
 import Login from "../pages/login";
 import Home from "../pages/home";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
-  },
-  {
-    path: "/login",
     element: <Login />,
   },
   {
